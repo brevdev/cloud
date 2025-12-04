cdd39b20093282de310725b8597fb6ed56122102
Brian Lechthaler <brianlechthaler@protonmail.ch>
2025-12-03 16:28:44 -0800

    implement GetLocations

--- v1/providers/sfcompute/instancetype.go
+++ v1/providers/sfcompute/instancetype.go
@@ -1,9 +1,37 @@
 package v1
 
 import (
+	"context"
 	"fmt"
+
+	v1 "github.com/brevdev/cloud/v1"
 )
 
 func (c *SFCClient) getInstanceTypeID(region string) string {
 	return fmt.Sprintf("h100v_%v", region)
+}
+
+func (c *SFCClient) GetLocations(ctx context.Context, _ v1.GetLocationsArgs) ([]v1.Location, error) {
+	resp, err := c.client.Zones.List(ctx)
+	if err != nil {
+		return nil, err
+	}
+	var locations map[string]v1.Location
+	if resp != nil {
+		for _, zone := range resp.Data {
+			var available = false
+			if len(zone.AvailableCapacity) != 0 && zone.DeliveryType == "VM" {
+				available = true
+				locations[zone.Name] = v1.Location{
+					Name:        zone.Name,
+					Description: string(zone.HardwareType),
+					Available:   available}
+			}
+		}
+	}
+	availableLocations := []v1.Location{}
+	for _, location := range locations {
+		availableLocations = append(availableLocations, location)
+	}
+	return availableLocations, nil
 }
