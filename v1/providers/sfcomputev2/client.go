5c9c3f07bf5369e8180f9744993f608b10fd5f21
Drew Malin <dmalin@nvidia.com>
2026-06-17 10:42:08 -0700

    use v2 in provider id

--- v1/providers/sfcomputev2/client.go
+++ v1/providers/sfcomputev2/client.go
@@ -7,7 +7,7 @@ import (
 	sfc "github.com/sfcompute/sfc-go"
 )
 
-const CloudProviderID = "sfcompute"
+const CloudProviderID = "sfcompute-v2"
 
 // SFCCredentialV2 holds authentication details for a Brev-managed SFCompute V2 account.
 type SFCCredentialV2 struct {
