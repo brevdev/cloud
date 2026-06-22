c9294306e36be93d379e37ee868509f0eaa9a8fe
Drew Malin <dmalin@nvidia.com>
2026-06-22 13:36:52 -0700

    lint

--- v1/providers/testkube/instance_test.go
+++ v1/providers/testkube/instance_test.go
@@ -52,7 +52,7 @@ func TestCreateInstanceProvisionFailures(t *testing.T) {
 	}
 }
 
-func TestInstanceLifecycle(t *testing.T) { //nolint:funlen // test ok
+func TestInstanceLifecycle(t *testing.T) {
 	ctx := context.Background()
 	client := newTestClient(t)
 
